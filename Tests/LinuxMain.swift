d1327ab98f389745eb5dc02a829e5abcee7f2472
Thomas G. Boop <thomasboop@Thomass-MacBook-Pro-3.local>
2017-06-19 15:00:36 -0500

    updated linuxmain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,16 +2,16 @@ import XCTest
 //@testable import WatsonDeveloperCloudTests
 @testable import AlchemyDataNewsV1Tests
 @testable import AlchemyLanguageV1Tests
-@testable import AlchemyVisionV1Tests
-@testable import ConversationV1Tests
-@testable import DialogV1Tests
+//@testable import AlchemyVisionV1Tests
+//@testable import ConversationV1Tests
+//@testable import DialogV1Tests
 @testable import DocumentConversionV1Tests
 @testable import LanguageTranslatorV2Tests
 @testable import NaturalLanguageClassifierV1Tests
-@testable import PersonalityInsightsV2Tests
+//@testable import PersonalityInsightsV2Tests
 @testable import RelationshipExtractionV1BetaTests
-@testable import RetrieveAndRankV1Tests
-@testable import TextToSpeechV1Tests
+//@testable import RetrieveAndRankV1Tests
+//@testable import TextToSpeechV1Tests
 @testable import ToneAnalyzerV3Tests
 @testable import TradeoffAnalyticsV1Tests
 @testable import VisualRecognitionV3Tests
@@ -20,16 +20,16 @@ XCTMain([
      //testCase(WatsonDeveloperCloudTests.allTests),
     testCase(AlchemyDataNewsTests.allTests),
     testCase(AlchemyLanguageTests.allTests),
-    testCase(AlchemyVisionTests.allTests),
-    testCase(ConversationTests.allTests),
-    testCase(DialogTests.allTests),
+    //testCase(AlchemyVisionTests.allTests),
+    //testCase(ConversationTests.allTests),
+    //testCase(DialogTests.allTests),
     testCase(DocumentConversionTests.allTests),
     testCase(LanguageTranslatorTests.allTests),
     testCase(NaturalLanguageClassifierTests.allTests),
-    testCase(PersonalityInsightsTests.allTests),
+    //testCase(PersonalityInsightsTests.allTests),
     testCase(RelationshipExtractionTests.allTests),
-    testCase(RetrieveAndRankTests.allTests),
-    testCase(TextToSpeechTests.allTests),
+    //testCase(RetrieveAndRankTests.allTests),
+    //testCase(TextToSpeechTests.allTests),
     testCase(ToneAnalyzerTests.allTests),
     testCase(TradeoffAnalyticsTests.allTests),
     testCase(VisualRecognitionTests.allTests)
