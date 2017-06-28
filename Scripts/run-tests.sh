83d613194e4b7efc630c4c8103d6777cfd55afef
Stephanie Cheng <sccheng@us.ibm.com>
2017-06-28 15:18:36 -0500

    Updated the destination name from $IOS10_DESTINATION to $IOS_DESTINATION

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -13,45 +13,45 @@ function uploadCodecov {
     bash <(curl -s https://codecov.io/bash)
 }
 
-xcodebuild build -scheme "RestKit" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" | xcpretty
+xcodebuild build -scheme "RestKit" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" | xcpretty
 checkStatus $?
-xcodebuild test -scheme "AlchemyDataNewsV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "AlchemyDataNewsV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "AlchemyLanguageV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "AlchemyLanguageV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "AlchemyVisionV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "AlchemyVisionV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "ConversationV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "ConversationV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "DialogV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "DialogV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "DiscoveryV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "DiscoveryV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "DocumentConversionV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "DocumentConversionV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "LanguageTranslatorV2" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "LanguageTranslatorV2" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "NaturalLanguageClassifierV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "NaturalLanguageClassifierV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "NaturalLanguageUnderstandingV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "NaturalLanguageUnderstandingV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "PersonalityInsightsV2" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "PersonalityInsightsV2" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "PersonalityInsightsV3" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "PersonalityInsightsV3" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "RelationshipExtractionV1Beta" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "RelationshipExtractionV1Beta" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "RetrieveAndRankV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "RetrieveAndRankV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "SpeechToTextV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "SpeechToTextV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "TextToSpeechV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "TextToSpeechV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "ToneAnalyzerV3" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "ToneAnalyzerV3" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "TradeoffAnalyticsV1" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES" | xcpretty
+xcodebuild test -scheme "TradeoffAnalyticsV1" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES" | xcpretty
 checkStatus $? && uploadCodecov
-xcodebuild test -scheme "VisualRecognitionV3" -sdk "$IOS_SDK" -destination "$IOS10_DESTINATION" -enableCodeCoverage "YES"
+xcodebuild test -scheme "VisualRecognitionV3" -sdk "$IOS_SDK" -destination "$IOS_DESTINATION" -enableCodeCoverage "YES"
 checkStatus $? && uploadCodecov
 
 exit $exitCode
